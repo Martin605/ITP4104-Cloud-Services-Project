f97099dfeb17a51a57d6fa20c9c89b878e3a9e7e
monty1990 <ec2-user@ip-172-31-80-95.ec2.internal>
2019-11-28 16:09:49 +0000

    cloud9_stack

--- cdk/cloud9_stack.py
+++ cdk/cloud9_stack.py
@@ -7,6 +7,7 @@ from aws_cdk import (
     core
     )
 
+
 class Cloud9Stack(core.Construct):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
@@ -27,7 +28,7 @@ class Cloud9Stack(core.Construct):
             constraint_description = "the password must be between 1 and 41 characters",
             default = "default"
         )
-       
+        
         #  EdxProjectCloud9
         edx_project_cloud9 = cloud9.CfnEnvironmentEC2(self, "EdxProjectCloud9",
             owner_arn = core.Fn.import_value("edXProjectUser"),
@@ -46,14 +47,26 @@ class Cloud9Stack(core.Construct):
         )
         
         '''
+        # S3 Bucket
         aws_account_id = core.Aws.ACCOUNT_ID
         source_bucket = "sourcebucketname%s" % (aws_account_id)
+        
+        s3_bucket = s3.Bucket(
+            self, "SourceBucket",
+            bucket_name = source_bucket
+            removal_policy = core.RemovalPolicy.DESTROY
+        )
+        
         # VPCStack
+        vpc_stack = cloudformation.CfnStack(self, "VPCStack",
+            template_url = 'https://%s.s3.amazonaws.com/vpc.yaml' % (source_bucket),
+            timeout_in_minutes = 5
+        )
+        # IAMStack
         iam_stack = cloudformation.CfnStack(self, "IAMStack",
             template_url = 'https://%s.s3.amazonaws.com/iam.yaml' % (source_bucket),
             timeout_in_minutes = 5,
             parameters = password_parameters
-        )
         '''
         
         # Output
