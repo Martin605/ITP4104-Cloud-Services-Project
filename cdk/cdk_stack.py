e77fb6fb6b68d0b7565cc1e7a1c7ce9370cea0e1
monty1990 <montyngan90@gmail.com>
2019-11-29 06:28:19 +0000

    cdk_stack

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -9,13 +9,41 @@ from aws_cdk import (
 from cdk.vpc_stack import VpcStack
 from cdk.iam_stack import IAMStack
 from cdk.cloud9_stack import Cloud9Stack
-from cdk.db_stack import DBStack
 class CdkStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
         
+        '''
+        # S3 Bucket
+        aws_account_id = core.Aws.ACCOUNT_ID
+        source_bucket = "sourcebucketname%s" % (aws_account_id)
+        
+        s3_bucket = s3.Bucket(
+            self, "SourceBucket",
+            bucket_name = source_bucket
+            removal_policy = core.RemovalPolicy.DESTROY
+        )
+        
+        # VPCStack
+        vpc_stack = cloudformation.CfnStack(self, "VPCStack",
+            template_url = 'https://%s.s3.amazonaws.com/vpc.yaml' % (source_bucket),
+            timeout_in_minutes = 5
+        )
+        # IAMStack
+        iam_stack = cloudformation.CfnStack(self, "IAMStack",
+            template_url = 'https://%s.s3.amazonaws.com/iam.yaml' % (source_bucket),
+            timeout_in_minutes = 5,
+            parameters = password_parameters
+        # Cloud9Stack
+        cloud9_stack = cloudformation.CfnStack(self, "Cloud9Stack",
+            template_url = 'https://%s.s3.amazonaws.com/cloud9.yaml' % (source_bucket),
+            timeout_in_minutes = 5,
+            parameters = {
+                "PublicSubnet1" : core.Fn.import_value("PublicSubnet1")
+            }
+        '''
+        
         Cloud9Stack(self, "Cloud9Stack")
         IAMStack(self, "IAMStack")
         VpcStack(self, "VpcStack")
-        
--- cdk/cloud9_stack.py
+++ cdk/cloud9_stack.py
@@ -14,22 +14,7 @@ class Cloud9Stack(core.Construct):
         super().__init__(scope, id, **kwargs)
         
         # Exercise 5
-        # Set Parameters
-        parameters = core.CfnParameter(self, "SourceBucket",
-            description = "Source Bucket with nested cloudformation template",
-            default = "default"
-        )
-        
-        password_parameters = core.CfnParameter(self, "Password",
-            no_echo = True,
-            description = "New account password",
-            min_length = 1,
-            max_length = 41,
-            constraint_description = "the password must be between 1 and 41 characters",
-            default = "default"
-        )
-        
-        #  EdxProjectCloud9
+        # EdxProjectCloud9
         edx_project_cloud9 = cloud9.CfnEnvironmentEC2(self, "EdxProjectCloud9",
             owner_arn = core.Fn.import_value("edXProjectUser"),
             description = "Building On AWS Cloud9",
@@ -45,29 +30,6 @@ class Cloud9Stack(core.Construct):
             bucket_name = "imagebucket%s" % (core.Aws.ACCOUNT_ID),
             removal_policy=core.RemovalPolicy.DESTROY
         )
-        
-        '''
-        # S3 Bucket
-        aws_account_id = core.Aws.ACCOUNT_ID
-        source_bucket = "sourcebucketname%s" % (aws_account_id)
-        
-        s3_bucket = s3.Bucket(
-            self, "SourceBucket",
-            bucket_name = source_bucket
-            removal_policy = core.RemovalPolicy.DESTROY
-        )
-        
-        # VPCStack
-        vpc_stack = cloudformation.CfnStack(self, "VPCStack",
-            template_url = 'https://%s.s3.amazonaws.com/vpc.yaml' % (source_bucket),
-            timeout_in_minutes = 5
-        )
-        # IAMStack
-        iam_stack = cloudformation.CfnStack(self, "IAMStack",
-            template_url = 'https://%s.s3.amazonaws.com/iam.yaml' % (source_bucket),
-            timeout_in_minutes = 5,
-            parameters = password_parameters
-        '''
         
         # Output
         core.CfnOutput(self, "EdxProjectCloud9Output",
