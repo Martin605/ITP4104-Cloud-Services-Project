c2669e2ef6cb34db68fa71ef9c0b2d19d212d262
Monty1990 <48192505+Monty1990@users.noreply.github.com>
2019-12-08 22:40:39 +0800

    Update cloud9_stack.py

--- cdk/cloud9_stack.py
+++ cdk/cloud9_stack.py
@@ -2,18 +2,22 @@ from aws_cdk import (
     aws_iam as iam,
     aws_cloud9 as cloud9,
     aws_lambda as lambda_,
+    aws_cloudformation as cfn,
     aws_ec2 as ec2,
     core
     )
     
-class Cloud9Stack(core.Construct):
+class Cloud9Stack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Exercise 5
+        # Exercise 9
         # Set Parameters
-        
+        # PublicSubnet1 = core.CfnParameter(self, "PublicSubnet1",
+        #     type = "AWS::EC2::Subnet::Id",
+        #     default =  "PublicSubnet1"
+        # )
         
         # LambdaExecutionRole
         lambda_execution_role = iam.CfnRole(self, "LambdaExecutionRole",
@@ -70,33 +74,55 @@ class Cloud9Stack(core.Construct):
         edx_project_cloud9.apply_removal_policy(core.RemovalPolicy.DESTROY)
         
         # CustomFunction
-        custom_function = lambda_.Function(self,"CustomFunction",
-            runtime = lambda_.Runtime.NODEJS_8_10,
-            handler = "custom_function.handler",
-            code = lambda_.Code.asset('./cdk/lambda'),
-            timeout = core.Duration.seconds(30),
-            role = iam.Role.from_role_arn(self, 'role_id', lambda_execution_role.attr_arn)
+        custom_function = lambda_.CfnFunction(self,"CustomFunction",
+            runtime = "nodejs8.10",
+            handler = "index.handler",
+            code = {
+"zipFile": "const response = require('cfn-response');\n"
+"const AWS = require('aws-sdk');\n"
+"exports.handler = (event, context) => {\n"
+    "let params = {\n"
+        "Filters: [\n"
+        "{\n"
+            "Name: 'tag:aws:cloud9:environment',\n"
+            "Values: [\n"
+                "event.ResourceProperties.EdxProjectCloud9\n"
+            "]\n"
+        "}\n"
+        "]\n"
+    "};\n"
+    "let ec2 = new AWS.EC2();\n"
+    "ec2.describeInstances(params, (err, data) => {\n"
+        "if (err) {\n"
+            "console.log(err, err.stack); // an error occurred\n"
+            "response.send(event, context, response.FAILED, {});\n"
+        "}else{\n"
+            "let responseData = {Value: data.Reservations[0].Instances[0].SecurityGroups[0].GroupId};\n"       
+            "console.log(responseData);\n"
+            "response.send(event, context, response.SUCCESS, responseData);\n"
+        "}\n"        
+    "});\n"
+"};"
+            },
+            timeout = 30,
+            role = lambda_execution_role.attr_arn
         )
         
         # CustomResource
-        # custom_resourcce = core.CfnResource(self, "CustomResource",
-        #     type = "Custom::CustomResource",
-        #     properties = {
-        #         "ServiceToken" : custom_function.function_arn,
-        #         "EdxProjectCloud9" : edx_project_cloud9.ref
-        #     }
-        # )
-        
+        custom_resource = cfn.CfnCustomResource( self, "CustomResource",
+            service_token = custom_function.attr_arn,
+        )
+        custom_resource.add_override("Properties.EdxProjectCloud9", edx_project_cloud9.ref)
         
         # Output
         core.CfnOutput(self, "EdxProjectCloud9Output",
-          value = edx_project_cloud9.ref,
-          description = "Edx User Cloud9",
-          export_name = "EdxProjectCloud9"
+            value = edx_project_cloud9.ref,
+            description = "Edx User Cloud9",
+            export_name = "EdxProjectCloud9"
         )
         
-        # core.CfnOutput(self, "EdxProjectCloud9SgOutput",
-        #   value = custom_resourcce.ref,
-        #   description = "Edx User Cloud9 Security Group ID",
-        #   export_name = "EdxProjectCloud9Sg"
-        # )
+        core.CfnOutput(self, "EdxProjectCloud9SgOutput",
+            value = core.Fn.get_att(custom_resource.logical_id, "Value").to_string(),
+            description = "Edx User Cloud9 Security Group ID",
+            export_name = "EdxProjectCloud9Sg"
+        )
