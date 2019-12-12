8e011889dca97a6152c9b81ede1bd7cc09fc2c37
monty <48192505+Monty1990@users.noreply.github.com>
2019-12-12 16:25:07 +0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "\033[36mCDK destory VpcStack\033[0m"
-cdk destory CDNStack  DBStack  
-echo -e "\033[36mCDK destory VpcStack\033[0m"
-cdk destory SecurityStack Cloud9Stack 
-echo -e "\033[36mCDK destory VpcStack IAMStack ParametesStack CognitoStack\033[0m"
-cdk destory VpcStack IAMStack ParametersStack CognitoStack
+echo -e "\033[36mCDK destroy CdnStack DBStack  \033[0m"
+cdk destroy CdnStack DBStack  
+echo -e "\033[36mCDK destroy SecurityStack Cloud9Stack \033[0m"
+cdk destroy SecurityStack Cloud9Stack 
+echo -e "\033[36mCDK destroy VpcStack IAMStack ParametesStack CognitoStack\033[0m"
+cdk destroy VpcStack IAMStack ParametersStack CognitoStack
 echo -e "\033[35mEND\033[0m"
